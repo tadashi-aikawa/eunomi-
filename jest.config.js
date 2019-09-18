6acb1d27aaf515fc65b4eaf5ab6f6150c85a5392
tadashi-aikawa <syou.maman@gmail.com>
2019-09-19 00:31:34 +0900

    Add tests

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  verbose: true,
+  collectCoverageFrom: ["src/**/*.ts"],
+  collectCoverage: true,
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"]
+};
---
+++ src/utils/time.test.ts
@@ -0,0 +1,14 @@
+import {toJapanese} from "./time";
+
+describe.each`
+timeStr       | expected
+${"00:00:01"} | ${"1秒"}
+${"00:00:59"} | ${"59秒"}
+${"00:01:00"} | ${"1分"}
+${"00:01:01"} | ${"1分"}
+${"00:59:59"} | ${"59分"}
+${"01:00:00"} | ${"1時間"}
+${"01:01:01"} | ${"1時間1分"}
+`("toJapanese", ({timeStr, expected}) => {
+  test(`${timeStr} -> ${expected}`, () => expect(toJapanese(timeStr)).toBe(expected))
+})
