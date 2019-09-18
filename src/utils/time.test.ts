79c0ef7b09b832840f689f3356429fc51b7db981
tadashi-aikawa <syou.maman@gmail.com>
2019-09-19 01:43:17 +0900

    Add tests (time#toSeconds)

--- src/utils/time.test.ts
+++ src/utils/time.test.ts
@@ -1,14 +1,27 @@
-import {toJapanese} from "./time";
+import { toJapanese, toSeconds } from './time';
 
 describe.each`
-timeStr       | expected
-${"00:00:01"} | ${"1秒"}
-${"00:00:59"} | ${"59秒"}
-${"00:01:00"} | ${"1分"}
-${"00:01:01"} | ${"1分"}
-${"00:59:59"} | ${"59分"}
-${"01:00:00"} | ${"1時間"}
-${"01:01:01"} | ${"1時間1分"}
-`("toJapanese", ({timeStr, expected}) => {
-  test(`${timeStr} -> ${expected}`, () => expect(toJapanese(timeStr)).toBe(expected))
-})
+  timeStr       | expected
+  ${'00:00:01'} | ${1}
+  ${'00:00:59'} | ${59}
+  ${'00:01:00'} | ${60}
+  ${'00:01:01'} | ${61}
+  ${'00:59:59'} | ${3599}
+  ${'01:00:00'} | ${3600}
+  ${'01:01:01'} | ${3661}
+`('toSeconds', ({ timeStr, expected }) => {
+  test(`${timeStr} -> ${expected}`, () => expect(toSeconds(timeStr)).toBe(expected));
+});
+
+describe.each`
+  timeStr       | expected
+  ${'00:00:01'} | ${'1秒'}
+  ${'00:00:59'} | ${'59秒'}
+  ${'00:01:00'} | ${'1分'}
+  ${'00:01:01'} | ${'1分'}
+  ${'00:59:59'} | ${'59分'}
+  ${'01:00:00'} | ${'1時間'}
+  ${'01:01:01'} | ${'1時間1分'}
+`('toJapanese', ({ timeStr, expected }) => {
+  test(`${timeStr} -> ${expected}`, () => expect(toJapanese(timeStr)).toBe(expected));
+});
