c0ec22144b4d74bf2d790d697bc48ac71cd85b03
tadashi-aikawa <syou.maman@gmail.com>
2019-12-22 23:05:53 +0900

    :heavy_check_mark: Fix collecting coverage failure

--- babel.config.js
+++ babel.config.js
@@ -3,5 +3,6 @@ module.exports = {
   plugins: [
     ['@babel/plugin-transform-typescript', { allowNamespaces: true }],
     '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator',
   ],
 };
